738e10f6908405e83cf799296d180b7ec30239b4
prachi-kurmi <125259987+prachi-kurmi@users.noreply.github.com>
2024-07-21 19:17:47 +0530

    (Activity_4) script.js

---
+++ 30daysjs/DOM_Manipulation/Activity_4/script.js
@@ -0,0 +1,14 @@
+//* Activity 4: Modifying Attributes and Classes */
+
+//* Task 7: Select an HTML element and change one of its attributes (e.g., src of an img tag) */
+
+const img = document.querySelector("#image")
+
+img.src = "https://preview.redd.it/satoru-gojo-reference-fanart-v0-e3zaq33rfn791.jpg?width=1080&crop=smart&auto=webp&s=3bdd2fbf44ed69231cdb671f2d43a8fa305a5a4a"
+
+//* Task 8: Add and remove a class to/from HTML element */
+
+document.getElementById('addbg').classList.add('bgcolor');
+
+document.getElementById('removebgcolor').classList.remove('removebg');
+
