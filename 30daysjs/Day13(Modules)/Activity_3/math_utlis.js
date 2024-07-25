799168ac58dd7add027c55049f25542c8bd3dc50
prachi-kurmi <125259987+prachi-kurmi@users.noreply.github.com>
2024-07-25 21:48:01 +0530

    Activity_3 (math_utlis.js)

---
+++ 30daysjs/Day13(Modules)/Activity_3/math_utlis.js
@@ -0,0 +1,12 @@
+// Task 5
+
+export const PI = 3.14;
+export const E = 2.71;
+
+export function square(num) {
+    return `Square of a number ${num} is: ${num*num}`;
+}
+
+export function cube(num) {
+    return `Cube of a number ${num} is: ${num*num*num}`;
+}
