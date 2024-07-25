0894ba01418d9cf1bedb3cdf4855c1cfab264425
prachi-kurmi <125259987+prachi-kurmi@users.noreply.github.com>
2024-07-25 21:40:19 +0530

    Activity_1 (math.js)

---
+++ 30daysjs/Day13(Modules)/Activity_1/math.js
@@ -0,0 +1,15 @@
+// Task 1 
+
+// 1st method
+// function add(num1, num2) {
+//     return (`Num 1: ${num1} addition with Num2: ${num2}:`,num1+num2) ;
+//   }
+  
+// module.exports = add;
+
+
+//* 2nd Method  */
+
+export function add(num1, num2) {
+  return (`Num 1: ${num1} addition with Num2: ${num2}: ${num1+num2}`) ;
+}
