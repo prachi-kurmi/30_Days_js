1b233582e63e5a28450a4937069b8b993fe6d379
prachi-kurmi <125259987+prachi-kurmi@users.noreply.github.com>
2024-07-25 21:44:45 +0530

    Activity_2 (math_functions.js)

---
+++ 30daysjs/Day13(Modules)/Activity_2/math_functions.js
@@ -0,0 +1,13 @@
+// Task 3
+
+export function add(num1, num2) {
+    return (`Num 1: ${num1} addition with Num2: ${num2}: ${num1+num2}`) ;
+}
+
+export function sub(num1, num2) {
+    return (`Num 1: ${num1} subtraction with Num2: ${num2}: ${num1-num2}`) ;
+}
+
+export function multiply(num1, num2) {
+    return (`Num 1: ${num1} multiplication with Num2: ${num2}: ${num1*num2}`) ;
+}
